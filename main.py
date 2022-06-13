332a5ae993d9c5aa3cace49ccd936341ab6cce64
ScottH1988 <scotthedges1988@gmail.com>
2022-06-13 14:16:12 +0100

    player image and reiszing code

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # creates the intial surface for the game
 # WIDTH,HEIGHT = WIN.get_size() # how to get the screen size of a user to use as the surface dimensions
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("Riftworld", "assets", "background.png")), (WIDTH, HEIGHT))
+PUG = pygame.transform.scale2x(pygame.image.load(os.path.join("Riftworld", "assets", "boy.png"))) # doubles the size of the images uing "pygame.transform.scale2x"
+
+
 
 # Game window name & icon
 pygame.display.set_caption("Welcome to Riftwold!")
 icon = pygame.image.load(os.path.join("Riftworld", "assets", "castle.png"))
 pygame.display.set_icon(icon)
 
-# Game background
-background = pygame.transform.scale(pygame.image.load(os.path.join("Riftworld", "assets", "background.png")), (WIDTH, HEIGHT))
+# Draw window function to diplay background and character images v1.0
 def draw_Window():
-    WIN.blit(background, (0,0))
-    pygame.display.update()
+    WIN.blit(BACKGROUND, (0,0))
+    
 
+    WIN.blit(PUG, (10, 420))
+    
+    pygame.display.update()
 
 
 def main():
