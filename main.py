0be8bec22f8f6969fe39d95bc0be8c1f911b3290
ScottH1988 <scotthedges1988@gmail.com>
2022-06-13 12:01:34 +0100

    intial setup of Riftworld pygame

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter.tix import WINDOW
+import pygame
+pygame.init()
+
+# Main Variables
+WIN = pygame.display.set_mode() # creates the intial surface for the game
+WIDTH,HEIGHT = WIN.get_size() # how to get the screen size of a user to use as the surface dimensions
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                run = False
+                pygame.quit()
+
+
+
+
+
+
+main()
+
+
+
+
+ 
