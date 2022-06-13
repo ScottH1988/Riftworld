e1a9f26bc0b6dee14b4e39b43ca03fa0ee387b0a
ScottH1988 <scotthedges1988@gmail.com>
2022-06-13 13:41:29 +0100

    game icon added as well as dev screen size

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+from turtle import width
 import pygame
+import os
 pygame.init()
 
 # Main Variables
-WIN = pygame.display.set_mode() # creates the intial surface for the game
-WIDTH,HEIGHT = WIN.get_size() # how to get the screen size of a user to use as the surface dimensions
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # creates the intial surface for the game
+# WIDTH,HEIGHT = WIN.get_size() # how to get the screen size of a user to use as the surface dimensions
+
+# Game window name & icon
+pygame.display.set_caption("Welcome to Riftwold!")
+icon = pygame.image.load(os.path.join("Riftworld", "assets", "castle.png"))
+pygame.display.set_icon(icon)
+
 
 def main():
     run = True
     while run:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT():
-                run = False
-                pygame.quit()
+            if event.type == pygame.QUIT:
+             run = False
+             pygame.quit()
 
-
+    
 
 
 
 
 main()
-
+if __name__ == "__main__":
+    main()
 
 
 
